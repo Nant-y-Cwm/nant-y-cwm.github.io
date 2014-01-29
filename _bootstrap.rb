8eb5190e69ffd082a9788431cdc5f3f4dc017b1f
Courtenay Probert <courtenay@probert.me.uk>
2014-01-29 18:45:09 +0000

    Latesat

--- _bootstrap.rb
+++ _bootstrap.rb
@@ -1,25 +1,23 @@
-
-ScmsUtils.log( "<div class='alert alert-info'><h4>Running Bootstrapper</h4></div>" )
+require_relative '_modules/FBEvents.rb'
 
 root = File.expand_path(File.dirname(__FILE__))
-modules = File.join(root, "_modules")
 
-Dir.glob(File.join(modules, "*.rb")).each do |f| 
-	ScmsUtils.log( "Requiring: #{File.basename(f)}" )
-	require_relative f
-end
+ScmsUtils.log( "<div class='alert alert-info'><h4>Running Bootstrapper</h4></div>" )
 
 #Get FB Events
 group_id = "124914354200526"
 app_id = "1406742256221468"
 app_secret = "b0faaf76456d30d04609779a4d8be0e9"
 event_view_resource = File.join($website, "_resources", "events.yml")
+
 begin 
 	if $watch
-    	puts "Skiping calendar scrape while in watch mode"
+    	puts "Skipping calendar scrape while in watch mode"
     else
     	FBEvents.get(group_id, app_id, app_secret, event_view_resource)  if !$cmsmode
 	end
-rescue r
+rescue Exception => e  
     puts "Error getting Facebook events"
+	puts e.message  
+    puts e.backtrace.inspect  
 end
