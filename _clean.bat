7b9e0169c1f4cd89530e7f008440a07d8dd33e66
Courtenay Probert <courtenay@probert.me.uk>
2015-01-09 08:13:48 +0000

    Removed compiled bundles

---
+++ _clean.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %0\..
+git clean -fdx
+pause
--- _publish.bat
+++ _publish.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d %0\.. 
+cd /d %0\..
+git reset
+git checkout .
 git pull origin
 scms --publish > .\last-deploy.log
-::start http://www.nant-y-cwm.co.uk/
-::pause
