d944907ed2bc4d856639b6272ab84ae3859d44fc
Courtenay Probert <courtenay@probert.me.uk>
2015-01-09 08:33:16 +0000

    Publish bat changes

--- _publish.bat
+++ _publish.bat
@@ -1,6 +1,14 @@
 @echo off
 cd /d %0\..
-git reset
-git checkout .
+
+echo Resetting
+@call git reset
+
+echo Checkout
+@call git checkout .
+
+echo Getting latest code
 @call git pull origin
+
+echo Deploying
 scms --publish > .\last-deploy.log
